3f5b0058ffbdcd379311f5c2be5d17d84c405960
Vincent Katz <vinz.k@live.fr>
2020-05-13 17:29:37 +0300

    finished part 4

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+
+    redirect_to cocktail
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-    get "cocktails", to: "cocktails#index"
-    get "cocktails", to: "cocktails#show"
-
+  get "cocktails", to: "cocktails#index", as: :cocktails
+  get "cocktails/new", to: "cocktails#new", as: :new_cocktail
+  post "cocktails", to: "cocktails#create"
+  get "cocktails/:id", to: "cocktails#show", as: :cocktail
+  delete "/doses/:id", to: "doses#destroy", as: :delete_dose
+  get "cocktails/:cocktail_id/doses/new", to: "doses#new", as: :new_dose
+  post "cocktails/:cocktail_id/doses", to: "doses#create", as: :cocktail_doses
 end
